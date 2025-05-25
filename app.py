64e1bd8482784dcbca815e7dd9d721b3f4607e39
vivek05nov <47934150+vivek05nov@users.noreply.github.com>
2025-05-25 20:20:49 +0530

    added headless chrome

--- app.py
+++ app.py
@@ -5,11 +5,19 @@ import time
 from platformdirs import user_downloads_dir
 from selenium import webdriver
 from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.Chrome()
+options = Options()
+options.binary_location = '/usr/bin/chromium-browser'  # important fix
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(service=Service(), options=options)
 
 
 def run_selenium(url: str, browser_type: str):
