5666e8bbb722b5807e20ce0a0baf3932fd596a03
vivek05nov <47934150+vivek05nov@users.noreply.github.com>
2025-07-20 16:50:36 +0530

    unused spaces

--- app.py
+++ app.py
@@ -445,4 +445,4 @@ if __name__ == "__main__":
     url = sys.argv[1] if len(sys.argv) > 1 else "https://magiclight.ai/"
     prompt = sys.argv[2] if len(sys.argv) > 2 else ""
     message = sys.argv[3] if len(sys.argv) > 3 else " "
-    run_selenium(url, prompt, message)
+    run_selenium(url, message)
